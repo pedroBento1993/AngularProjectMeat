ac1f69c46a8f1284dff36862c03bf473bcb93f41
pedro bento <pbento@innovagency.com>
2019-05-21 18:04:58 +0100

    video 51

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html',
+  styleUrls: ['./restaurant-detail.component.css']
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  restaurant : Restaurant
+
+  constructor(private restaurentaService : RestaurantsService,
+    private route : ActivatedRoute
+    ) { }
+
+  ngOnInit() {
+    this.restaurentaService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
+  }
+
+}
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,29 +1,40 @@
+import { Injectable } from '@angular/core'
+import { Http } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
 import { Restaurant } from "app/restaurants/restaurant/restaurant.model";
+import { MEAT_API } from "../app.api";
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import { ErrorHandler } from 'app/app.error-handler';
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
-
+@Injectable()
 export class RestaurantsService {
-    rests: Restaurant[] = [
-        {
-            id: "bread-bakery",
-            name: "Bread & Bakery",
-            category: "Bakery",
-            deliveryEstimate: "25m",
-            rating: 4.9,
-            imagePath: "assets/img/restaurants/breadbakery.png"
-        },
-        {
-            id: "burger-house",
-            name: "Burger House",
-            category: "Hamburgers",
-            deliveryEstimate: "100m",
-            rating: 3.5,
-            imagePath: "assets/img/restaurants/burgerhouse.png"
 
-        }]
 
-    constructor() { }
+    constructor(private http: Http) { }
 
-    restaurants(): Restaurant[] {
-        return this.rests;
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    reviewsOfRestaurants(id: string): Observable<any> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
+    menuOfRestaurants(id: string): Observable<MenuItem[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
     }
 }
