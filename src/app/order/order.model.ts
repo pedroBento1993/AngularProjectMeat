db87420fb9af83a59f464af4744e204f982b5646
pbento <pbento@innovagency.com>
2019-05-23 23:24:49 +0100

    video 68

---
+++ src/app/order/order.model.ts
@@ -0,0 +1,17 @@
+
+
+class Order {
+    constructor(
+        public endereco: string,
+        public numero: number,
+        public optionalAddress: string,
+        public paymentOption: string,
+        public orderItems: OrderItem[] = []) { }
+
+}
+
+class OrderItem {
+    constructor(public quantity: number, public menuId: string) { }
+}
+
+export { Order, OrderItem }
